8478ea2bc212f978556041f2f520c197524ddbb9
GEVORGYAN-VAHE <vahegevorgyan1987@gmail.com>
2022-11-03 20:13:05 +0400

    first

---
+++ das35.js
@@ -0,0 +1,100 @@
+// var b = document.getElementsByTagName("button")
+// e[0].style.background = "red"
+
+
+// 1.. buttonin klick aneluc button-i guyny poxvi,
+// buttonin klick aneluc body-i guyny poxvi patahakan guynerov
+
+
+// function f(){
+//    var e = document.getElementsByTagName("body")
+
+//     e[0].style.background = "red"
+//     var r = Math.round(Math.random()*255)
+//     var g = Math.round(Math.random()*255)
+//     var b = Math.round(Math.random()*255)
+//     e[0].style.background = ` rgb( ${r}, ${g}, ${b} ) `
+// }
+
+
+//  // 2.  inputi value-in kpnelu dzev
+// function f(){
+//     var input = document.getElementById("inp").value
+//     alert(input)
+// }
+
+
+
+// // 3. unenq input ev button, ete inch vor ban grenq inputum ev sexmenq buttonin, apa body-um khaytnvi inputi valuen
+
+// function f(){
+//     var input = document.getElementsByTagName("input")[0].value
+//     var element = document.getElementsByTagName("h1")
+//     element[0].innerHTML = input
+// }
+
+            //   T  N  A  Y  I  N
+
+// // 4. unenq button ev input,inch tiv grenq inputum aydqan div stexci 200*200 vra
+
+// function f(){
+//   var input = document.getElementsByTagName("input")[0].value
+
+//   for(var i = 0; i < input; i++){
+//     var e = document.createElement("div")
+//     e.style.width = "200px"
+//     e.style.height = "200px"
+//     e.style.border = "1px solid"
+//     document.body.appendChild(e)
+//   } 
+// }
+
+
+// // 5.  unenq 2 input 4 hat button
+// // inputnerum inch tver vor grenq amen mi button katari mat gorcoxutyunner (+-*)
+
+
+// function f1(){
+//     var input1 = +document.getElementById("inp1").value;
+//     var input2 = +document.getElementById("inp2").value;
+//     console.log(input1 + input2)
+// }
+
+// function f2(){
+//     var input1 = +document.getElementById("inp1").value;
+//     var input2 = +document.getElementById("inp2").value;
+//     console.log(input1 - input2)
+// }
+
+// function f3(){
+//     var input1 = +document.getElementById("inp1").value;
+//     var input2 = +document.getElementById("inp2").value;
+//     console.log(input1 * input2)
+// }
+
+// function f4(){
+//     var input1 = +document.getElementById("inp1").value;
+//     var input2 = +document.getElementById("inp2").value;
+//     console.log(input1 / input2)
+// }
+
+
+// 6. unenq 4 hat input 1 hat knopka , input N1 width,N2 height,N3 border,N4 qanak, u henc sexmenq knopkin aydpisi div stexcvi
+
+// function f(){
+//   var input1 = document.getElementById("inp1").value;
+//   var input2 = document.getElementById("inp2").value;
+//   var input3 = document.getElementById("inp3").value;
+//   var input4 = document.getElementById("inp4").value;
+
+//   for(var i = 0; i<input4; i++){
+//     var e = document.createElement("div")
+//     e.style.width = input1 +"px"
+//     e.style.height = input2 + "px"
+//     e.style.border = input3 +"px solid red"
+//     document.body.appendChild(e)
+//   }
+// }
+
+
+
